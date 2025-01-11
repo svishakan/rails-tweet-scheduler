86efd2a0f256de43cf59662a9f73991e79b50361
Vishakan Subramanian <svishakan123@yahoo.com>
2025-01-11 20:21:16 +0000

    Delete Tweets when deleting Twitter account

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -1,5 +1,5 @@
 class TwitterAccount < ApplicationRecord
-  has_many :tweets
+  has_many :tweets, dependent: :destroy # If a TwitterAccount is deleted, delete all associated Tweets
   belongs_to :user
 
   validates :username, uniqueness: true
